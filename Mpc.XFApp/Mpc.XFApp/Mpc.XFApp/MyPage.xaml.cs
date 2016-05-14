2d27409d00eea084cc7d6458a8d7cd4fc2baa512
Miguel Costa <miguelpintodacosta@gmail.com>
2016-05-15 00:56:49 +0100

    Add project with xaml page

---
+++ Mpc.XFApp/Mpc.XFApp/Mpc.XFApp/MyPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Mpc.XFApp
+{
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSlideColorChanged(object sender, EventArgs e)
+        {
+            var red = sliderRed.Value;
+            var green = sliderGreen.Value;
+            var blue = sliderBlue.Value;
+
+            boxviewColor.Color = Color.FromRgb(red, green, blue);
+        }
+    }
+}
