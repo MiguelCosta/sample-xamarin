f7e68d662c085bf4505ff570ddb00e560ce64c5a
Miguel Costa <miguelpintodacosta@gmail.com>
2016-05-14 22:06:58 +0100

    Update packages

--- XFApp/XFApp/XFApp.Droid/MainActivity.cs
+++ XFApp/XFApp/XFApp.Droid/MainActivity.cs
@@ -1,24 +1,18 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace XFApp.Droid
 {
-	[Activity (Label = "XFApp", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
-	{
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
+    [Activity(Label = "XFApp", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-			global::Xamarin.Forms.Forms.Init (this, bundle);
-			LoadApplication (new XFApp.App ());
-		}
-	}
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new XFApp.App());
+        }
+    }
 }
-
